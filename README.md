08fab92c792c700ea9c14479c826107e8c0e30e8
mathieu-ai <mathieu.lievre@efrei.net>
2022-12-28 14:59:54 +0000

    feat ; - excel-to-json

---
+++ README.md
@@ -0,0 +1,49 @@
+# excel-to-json
+
+[![npm version][npm-badge]][npm-url] [![Open issues][issues-badge]][issues-url] [![TypeScript][typescript-badge]][typescript-url]
+
+🚩 Transform your excel into a json
+
+## Install
+
+```bash
+npm i excel-to-json.mlai
+```
+
+or
+
+```bash
+yarn add excel-to-json.mlai
+```
+
+or
+
+```bash
+pnpm i excel-to-json.mlai
+```
+
+## Example
+
+```js
+import { $ExcelToJson } from 'excel-to-json.mlai';
+
+console.log($ExcelToJson(file));
+
+// prints: [{...}, {...}, {...}...]
+```
+
+## Contributing
+
+All contributions are welcome!
+
+[npm-url]: https://www.npmjs.com/package/generic-functions.mlai
+[npm-badge]: https://img.shields.io/node/v/generic-functions?style=for-the-badge
+[size-badge]: https://badgen.net/bundlephobia/Mathieu-ai/ExcelToJson
+[issues-badge]: https://img.shields.io/github/issues/Mathieu-ai/ExcelToJson?style=for-the-badge
+[issues-url]: https://github.com/Mathieu-ai/ExcelToJson/issues
+[typescript-badge]: https://badges.frapsoft.com/typescript/code/typescript.svg?v=101
+[typescript-url]: https://github.com/microsoft/TypeScript
+
+## License
+
+excel-to-json.mlai is [MIT licensed](LICENSE).
---
+++ src/library/Properties.ts
@@ -0,0 +1,82 @@
+import XLSX from 'xlsx';
+
+/**
+ * * Convert an excel file to an array of jsons & call function to transform props and values
+ *  * Example :
+ * ```js
+ * $ExcelToJson(pathtoExcelFile)
+ * // Prints: [{...},...]
+ * ```
+ * Convert an excel file to an array of jsons
+ * * 🚫 - needs a 'code' prop (aka:index)
+ * * ⚠️ - calls $removeBreakLines from [generic-functions.mlai](https://www.npmjs.com/package/generic-functions.mlai)
+ * * ✔️ - can take any excel file
+ * ___
+ * ```js
+ * const tbMembers = $ExcelToJson('path/to/file/EXCEL.xlsx')
+ * console.log(tbMembers);
+ * // Prints: [{...}, {...}]
+ * ```
+ * ___
+ * @param {String} pathOfFile
+ * @returns object[]
+ */
+export function $ExcelToJson(pathOfFile: any) {
+    const tbData: any[] = [];
+    const file = XLSX.readFile(pathOfFile, {
+        cellDates: true,
+        sheetStubs: true
+    });
+
+    const list_sheets = file.SheetNames;
+
+    // y = sheet Name
+    list_sheets.forEach((y: string | number) => {
+        const worksheet = file.Sheets[y];
+        // headers = object of properties
+        const headers: any = {};
+        let val: any;
+
+        for (const z in worksheet) {
+            if (z[0] === '!' || !z[0]) {
+                continue;
+            }
+            //parse column, row, and val
+            let tt = 0;
+            for (let i = 0; i < z.length; i++) {
+                // z = row name (aka:'A1')
+                const zi: any = z[i];
+                if (!isNaN(zi)) {
+                    // tt = index
+                    tt = i;
+                    break;
+                }
+            }
+            // get column letter (aka: 'A')
+            const col = z.substring(0, tt);
+            // get row number (aka: 1)
+            const row = parseInt(z.substring(tt));
+            // get prop name (aka: 'code')
+            val = worksheet[z].v;
+
+            //header names
+            if (row == 1) {
+                // if val have '\n' in the string
+                if (val) headers[col] = $removeBreakLines(val);
+                continue;
+            }
+            if (!val || val == '') val = false;
+            // because some row have x or X as values
+            if (val == 'x' || val == 'X') val = true;
+            // tbData[row] = object identifier (aka: {code: 6})
+            if (!tbData[row]) tbData[row] = {};
+            // value of column
+            tbData[row][headers[col]] = val;
+        }
+
+        //remove first two rows because empty
+        tbData.shift();
+        tbData.shift();
+    });
+    return tbData.filter((row) => row.code);
+}
